56fbeb80bbd3527d5842c6d325b9239379349f16
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-08 10:36:48 +0200

    Create ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,10 @@
+let rectangle = document.querySelector('#square');
+
+function reduceOpacity() {
+rectangle.style.opacity = "0.5";
+}
+function resetOpacity() {
+rectangle.style.opacity = "1";
+ }
+rectangle.addEventListener("mouseover", reduceOpacity)
+rectangle.addEventListener("mouseout", resetOpacity)
